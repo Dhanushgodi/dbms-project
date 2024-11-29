cb4f7449f5687eea42dcc3e3f5ef7f514adac084
dhanushgodi <dhanushgodi@gmail.com>
2024-11-29 11:13:29 +0530

    save changes

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'user',
-    password: 'pass',
+    user: 'postgres',
+    password: '2353',
     host: 'localhost',
     port: 5432,
     database: 'bank'
